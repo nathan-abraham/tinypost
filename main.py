d740c539075715fcbdddec61d3d9276f31d24280
Nathan Abraham <abnathan123@gmail.com>
2021-07-21 19:35:27 -0400

    Fixed indentation

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import tinypost as tp
 
 # while True:
-# 	expr = input("math shell > ") 
+# 	expr = input("math shell > ")
 # 	if expr == "exit" or expr == "quit":
 # 		break
-	
+
 # 	try:
 # 		result = tp.eval_expr(expr)
 # 		print(result)
@@ -20,9 +20,11 @@ import tinypost as tp
 # print(tp.eval_compiled(expr))
 # print(tp.eval_expr("3 + 12 / 2"))
 
+
 def my_weird_sum(a, b):
-	weird_num = 2.3551
-	return (a / weird_num) ** 2 + b
+    weird_num = 2.3551
+    return (a / weird_num) ** 2 + b
+
 
 tp.add_func("my_weird_sum", my_weird_sum)
 expr = tp.compile_expr("my_weird_sum(3, 9.2) + 3")
